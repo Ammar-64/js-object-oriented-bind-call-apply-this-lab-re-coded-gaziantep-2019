f47b62cbe887758cf1f7d7a55286768a4cf23864
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-22 12:53:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg)
 }
 function setThisWithApply(fn, thisValue, args){
-  fn.apply(thisValue, [args])
+  fn.apply(thisValue, args)
 }
